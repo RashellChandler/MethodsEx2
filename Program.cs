56a0fe8b803282b33cb10c6c6dede847e753b502
RashellChandler <rashell.chandler@yahoo.com>
2023-09-30 17:07:28 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace MethodsEx2
+{
+    class Program
+    {
+        public static int Sum(int num1, int num2)
+        {
+            var answer = num1 + num2;
+            return answer;
+        }
+        public static int Multiply(int num1, int num2, int num3)
+        {
+            return num1 * num2 * num3;
+        }     
+            static void Main(string[] args)
+        {
+            var amountOfCars = Sum(2, 6);
+            var blah = Multiply(60, 2, 4);
+        
+        }
+    }
+}
